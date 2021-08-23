551a4c80d7f5eb8fb965ea8bbe29415e1af88206
Vishal Talwar <vtalwar.w@gmail.com>
2021-08-23 14:16:16 -0700

    Adds home and details template

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import titleize from 'titleize';
 
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -10,14 +15,101 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const ListAttribute = ({name, label, type, values}) => {
+    return (
+        <Grid container>
+            <Grid item xs={2}>
+                <h4>{`${label}: `}</h4>
+            </Grid>
+            <Grid item xs={10}>
+                <Grid container>
+                    {values.map(val => (
+                        <Grid key={`${name}-${type ? val[type].name : val.name}`} item xs={2}>
+                            <h4>{`${type ? titleize(val[type].name) : titleize(val.name)}`}</h4>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
+
+const Attribute = ({ label, value }) => {
+    return (
+        <Grid container>
+            <Grid item xs={4}>
+                <h4>{`${label}: `}</h4>
+            </Grid>
+            <Grid item xs={8}>
+                <h4>{`${value ? value : ''}`}</h4>
+            </Grid>
+        </Grid>
+    );
+}
+
 const Details = () => {
     const classes = useStyles();
+
+    const handleChange = (event) => {
+        console.log(event.target.checked);
+    };
 
     return (
         <div className={classes.root}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                    <Typography align="center">{"Details"}</Typography>
+                    <Link to="/">
+                        <Typography align="center">{"Home"}</Typography>
+                    </Link>
+                </Grid>
+                <Grid item xs={5}>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography align="center">{`${titleize('detail')}`}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                    component="img"
+                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`}
+                                    title={titleize('detail')}
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Grid container>
+                                <Grid item xs={4}>
+                                    {<Attribute label={`Height`} value={`Height`}/>}
+                                </Grid>
+                                <Grid item xs={4}>
+                                    {<Attribute label={`Weight`} value={`Weight`}/>}
+                                </Grid>
+                                <Grid item xs={4}>
+                                    {<Attribute label={`Species`} value={`Species`}/>}
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <h4>{`Save: `}</h4>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Checkbox
+                                        checked={false}
+                                        onChange={handleChange}
+                                        inputProps={{ 'aria-label': 'save' }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    {<ListAttribute name={'name'} label={`Abilities`} type={`ability`} values={[{ability: {name: 'kungfu'}}]}/>}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={7}>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography align="center">{`${titleize('location')}`}</Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,75 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
-    }
+    },
+    header: {
+        margin: theme.spacing(1),
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    headerItem: {
+        margin: theme.spacing(1)
+    },
 }));
 
 const Home = () => {
     const classes = useStyles();
 
+    const handleChange = (event) => {
+        console.log(event.target.checked);
+    };
+
+    const handleSearch = (event) => {
+        console.log(event.target.value);
+    };
+
     return (
         <div className={classes.root}>
-            <Grid container spacing={3}>
+            <Grid container justifyContent="center" alignItems="center">
                 <Grid item xs={12}>
-                    <Typography align="center">{"Home"}</Typography>
+                    <Grid container className={classes.header}>
+                        <Grid item className={classes.headerItem}>
+                            <FormGroup row>
+                                <FormControlLabel
+                                    control={
+                                    <Switch
+                                        checked={false}
+                                        onChange={handleChange}
+                                        name="showSaved"
+                                        color="primary"
+                                    />
+                                    }
+                                    label="Show Saved"
+                                />
+                            </FormGroup>
+                        </Grid>
+                        <Grid item className={classes.headerItem}>
+                            <form noValidate autoComplete="off">
+                                <TextField 
+                                    id="search"
+                                    label="Search field"
+                                    value={''}
+                                    type="search"
+                                    inputProps={{ 'aria-label': 'search' }}
+                                    onChange={handleSearch} />
+                            </form>
+                        </Grid>
+                    </Grid>
+                    <Grid container>
+                        {[1,2,3,4,5,6,7,8,9,0,11].map(poke => (
+                            <Grid key={poke} item>
+                                <Typography align="center">{poke}</Typography>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Grid>
             </Grid>
         </div>
