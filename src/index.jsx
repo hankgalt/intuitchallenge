9258bb68476bb9d556ea6f0eca107fe0e45c28b2
Vishal Talwar <vtalwar.w@gmail.com>
2021-08-23 14:23:05 -0700

    Adds redux and sets up store

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from './containers/App'
+import { store } from './utils/store';
+import App from './containers/App';
 
-render(<App/>, document.getElementById("root"))
+render(<Provider store={store}><App/></Provider>, document.getElementById("root"))
---
+++ src/utils/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const loggerMiddleware = createLogger();
+
+export const store = createStore(
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+);
